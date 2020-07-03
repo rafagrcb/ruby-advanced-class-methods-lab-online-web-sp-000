215eeeb63405bac64f224074cf856a724ebaaae4
Rafael Garcia <rafagrcb@gmail.com>
2020-07-03 01:02:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,10 @@ class Song
   end
   
   def self.new_by_name(song_name)
-    new_song = self
+    new_song = self.new
+    song.name = name
+    song
+  end
   
   def self.create_by_name(song_name)
     song = self.new
