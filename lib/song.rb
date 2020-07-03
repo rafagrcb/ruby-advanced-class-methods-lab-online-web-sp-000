4fa5c6ff5b886e3bf35ffaaead44650f2baf96f2
Rafael Garcia <rafagrcb@gmail.com>
2020-07-03 01:07:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,5 +32,9 @@ class Song
   def self.find_by_name(song_name)
     self.all.detect { |song| song.name = song_name}
   end
+  
+  def self.fin_or_create_by_name(song_name)
+    self.all.find { |song| song.name = song_name}
+    
 
 end
