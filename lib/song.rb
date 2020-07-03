d6b51ebbc15eca2d9b4172e8bf97b5d16875475f
Rafael Garcia <rafagrcb@gmail.com>
2020-07-03 01:23:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,6 +49,9 @@ class Song
   end
   
   def self.create_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
     
     
 
