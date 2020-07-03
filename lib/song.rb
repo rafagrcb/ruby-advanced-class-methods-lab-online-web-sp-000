6a855c2079329d9c7f7cf6a037a6cdcf51ec783e
Rafael Garcia <rafagrcb@gmail.com>
2020-07-03 01:24:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,8 @@ class Song
     new_song = self.new
     new_song.name = file_name.split(" - ")[1].split(".")[0]
     new_song.artist_name = file_name.split(" - ")[0]
-    @@all << 
+    @@all << new_song
+  end
     
     
 
